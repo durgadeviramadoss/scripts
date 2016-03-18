cbe86e96ccb349df66c24a45a4b4f0797ae15057
durgadeviramadoss <durgaoct1989@gmail.com>
2016-03-18 11:34:00 +0530

    Added files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo -i
+yum -y update
+cd /opt/
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
+"http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jre-8u60-linux-x64.rpm"
+yum localinstall jre-8u60-linux-x64.rpm -y
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
+"http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-linux-x64.rpm"
+yum localinstall jdk-8u60-linux-x64.rpm -y
+
